a2949fc42401795fb193515e03a85c378e634287
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-01-18 23:26:12 +0700

    Update AuthRequest.java

--- src/main/java/broteam/myfap/backend/Dto/Auth/AuthRequest.java
+++ src/main/java/broteam/myfap/backend/Dto/Auth/AuthRequest.java
@@ -3,8 +3,7 @@ package broteam.myfap.backend.Dto.Auth;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+
 @AllArgsConstructor
 @Getter
 public class AuthRequest {
