150d24796e28fbbbc946f5f8dcbaf65d867a3a13
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-04 19:14:11 +0700

    edit secutiry config for submajor

--- src/main/java/broteam/myfap/backend/Config/SecurityConfiguration.java
+++ src/main/java/broteam/myfap/backend/Config/SecurityConfiguration.java
@@ -57,6 +57,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET, "/api/v1/submajor").hasAnyAuthority(RoleType.ADMIN.name())
                         .requestMatchers(HttpMethod.POST, "/api/v1/submajor").hasAnyAuthority(RoleType.ADMIN.name())
                         .requestMatchers(HttpMethod.GET, "/api/v1/submajor/search").hasAnyAuthority(RoleType.ADMIN.name())
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/submajor/*").hasAnyAuthority(RoleType.ADMIN.name())
 
 
                         .requestMatchers("/admin/auth").hasAnyAuthority(RoleType.ADMIN.name())
