599d281e39d8bb180a82c92713554cead5467b95
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-09 17:05:28 +0700

    Interface for service in room

---
+++ src/main/java/broteam/myfap/backend/Service/Unit/IRoomService.java
@@ -0,0 +1,11 @@
+package broteam.myfap.backend.Service.Unit;
+
+import broteam.myfap.backend.Dto.Unit.ClassDto;
+import broteam.myfap.backend.Dto.Unit.ClassRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface IRoomService {
+
+}
