9de80532cc62325b94aa47d886c99bc05ff13bbe
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-09 17:40:57 +0700

    Add room controller base and add to finadllbase

---
+++ src/main/java/broteam/myfap/backend/Controllers/Unit/RoomController.java
@@ -0,0 +1,35 @@
+package broteam.myfap.backend.Controllers.Unit;
+
+import broteam.myfap.backend.Dto.ResponseObject;
+import broteam.myfap.backend.Dto.Unit.ClassDto;
+import broteam.myfap.backend.Dto.Unit.ClassRequest;
+import broteam.myfap.backend.Dto.Unit.RoomDto;
+import broteam.myfap.backend.Exception.Unit.SchoolException;
+import broteam.myfap.backend.Service.Unit.ClassService;
+import broteam.myfap.backend.Service.Unit.RoomService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/unit/room")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
+public class RoomController {
+    private final RoomService roomService;
+
+    @GetMapping
+    public ResponseEntity<ResponseObject> getAllRooms() {
+        List<RoomDto> allRooms = roomService.findAllBase();
+        return ResponseEntity.ok(ResponseObject.builder()
+                .data(allRooms)
+                .message("Get successful")
+                .responseCode(HttpStatus.OK.value())
+                .build());
+    }
+
+}
