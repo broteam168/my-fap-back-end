61e5fc5eee3b720d9eda0a3a4b515967343446e7
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-09 17:03:35 +0700

    Add enity Room.java

---
+++ src/main/java/broteam/myfap/backend/Models/Unit/Room.java
@@ -0,0 +1,33 @@
+package broteam.myfap.backend.Models.Unit;
+
+import broteam.myfap.backend.Models.Enums.BuildingName;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "U_Room")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "Name")
+    private String Name;
+
+    @Column(name = "Description")
+    private String Description;
+
+    @Column(name = "Type")
+    private String Type;
+    @Column(name = "Building")
+    private BuildingName Building;
+
+    @Column(name = "IsActive")
+    private boolean IsActive;
+}
