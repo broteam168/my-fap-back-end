221d5cd738fd80bb45a5c6bd01e6d4a550816fb8
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-09 17:05:18 +0700

    Add repository for comunicating with sql

---
+++ src/main/java/broteam/myfap/backend/Repository/Unit/RoomRepository.java
@@ -0,0 +1,10 @@
+package broteam.myfap.backend.Repository.Unit;
+
+
+import broteam.myfap.backend.Models.Unit.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+}
