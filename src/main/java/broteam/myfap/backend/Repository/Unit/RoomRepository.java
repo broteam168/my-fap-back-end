3cd5e5afd65083f7877d2eec90475e80e09461cf
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-09 18:25:39 +0700

    Add findRoomBySchoolId

--- src/main/java/broteam/myfap/backend/Repository/Unit/RoomRepository.java
+++ src/main/java/broteam/myfap/backend/Repository/Unit/RoomRepository.java
@@ -3,8 +3,12 @@ package broteam.myfap.backend.Repository.Unit;
 
 import broteam.myfap.backend.Models.Unit.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface RoomRepository extends JpaRepository<Room, Integer> {
-
+    @Query("select c from Room c where c.SchoolId = :id")
+    List<Room> findBySchoolId(int  id);
 }
