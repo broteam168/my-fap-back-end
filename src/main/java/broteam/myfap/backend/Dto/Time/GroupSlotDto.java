b7ff1ae2d29a0e21aaa0ac150beb21ba7bd7c29a
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-19 16:34:33 +0700

    Add dto GroupSlot

---
+++ src/main/java/broteam/myfap/backend/Dto/Time/GroupSlotDto.java
@@ -0,0 +1,16 @@
+package broteam.myfap.backend.Dto.Time;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GroupSlotDto {
+    private int id;
+    private String Name;
+     private String Description;
+}
