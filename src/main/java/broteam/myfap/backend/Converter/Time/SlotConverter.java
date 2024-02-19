fc7bfd63e5a846d0665d26d93310c8d8a4373d6f
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-19 16:41:48 +0700

    Add converter for group slot

--- src/main/java/broteam/myfap/backend/Converter/Time/SlotConverter.java
+++ src/main/java/broteam/myfap/backend/Converter/Time/SlotConverter.java
@@ -4,7 +4,9 @@ import broteam.myfap.backend.Dto.Time.GroupSlotDto;
 import broteam.myfap.backend.Dto.Unit.SchoolDto;
 import broteam.myfap.backend.Models.Time.GroupSlot;
 import broteam.myfap.backend.Models.Unit.School;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SlotConverter {
     public GroupSlotDto toDto(GroupSlot entity){
         GroupSlotDto dto = new GroupSlotDto();
