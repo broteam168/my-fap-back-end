20f19d23e2768f65145d557c9237826800337af6
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-19 16:42:02 +0700

    Add find all base for group slot service

--- src/main/java/broteam/myfap/backend/Service/Time/IGroupSlotService.java
+++ src/main/java/broteam/myfap/backend/Service/Time/IGroupSlotService.java
@@ -2,10 +2,12 @@ package broteam.myfap.backend.Service.Time;
 
 import broteam.myfap.backend.Dto.Major.MajorDto;
 import broteam.myfap.backend.Dto.Major.MajorRequestDto;
+import broteam.myfap.backend.Dto.Time.GroupSlotDto;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface IGroupSlotService {
 
+    List<GroupSlotDto> findAllBase();
 }
