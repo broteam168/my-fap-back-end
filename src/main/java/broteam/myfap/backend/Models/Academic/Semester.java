acec483ec71cde3e5ef1ee7fa28eaabd78ab9918
Nguyễn Hoàng Sơn <sonnhhe186586@fpt.edu.vn>
2024-02-20 10:08:32 +0700

    Add enitity Semester

---
+++ src/main/java/broteam/myfap/backend/Models/Academic/Semester.java
@@ -0,0 +1,46 @@
+package broteam.myfap.backend.Models.Academic;
+
+import broteam.myfap.backend.Models.Enums.MajorCategory;
+import broteam.myfap.backend.Models.Major.SubMajor;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "A_Semester")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Semester {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "Name")
+    private String Name;
+
+     @Column(name = "Year")
+    private int Year;
+
+    @Column(name = "Order")
+    private int Order;
+
+    @Column(name = "StartDate")
+    private Date FullName;
+
+    @Column(name = "EndDate")
+    private Date EndDate;
+
+    @Column(name = "Description")
+    private String Description;
+
+    @Column(name = "IsActive")
+    private boolean IsActive;
+
+
+}
